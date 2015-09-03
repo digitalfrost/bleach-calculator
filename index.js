7470afbbb5f99eb80780cd455572797231e21d4b
Gerald <gerald.hofmaier@gmail.com>
2015-09-03 21:49:35 +0200

    default to 24% manufacturing concentration

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var MathJS = require('mathjs');
 
 //expects a hash with 3 of the 4 following keys :originalVolume, :originalConcentration, :finalVolume, :finalConcentration
+//an optional option specifyinf the sodiun hypochlorite concentration used to make the original bleach can be passed in.
 var BleachSolution = function (options) {
     this.originalVolume = options.originalVolume ;
     this.originalConcentration = options.originalConcentration;
     this.finalVolume = options.finalVolume;
     this.finalConcentration = options.finalConcentration;
-
+    this.manufacturingConcentration = options.manufacturingConcentration || 24 ; // default to 24% as per recommendation of C.S.F.E.J.
     //la densité moyenne pour un produit obtenu à partir d’un hypochlorite de sodium à 13 %
     //Average density of the original solution for a product originaly made from Sodium Hypochlorite at 13 %
     //This is an average density because in practice it can varry in function of the quality of the Sodium Hypochlorite
@@ -14,7 +15,7 @@ var BleachSolution = function (options) {
     //data comes from the French "Chambre Syndicale des fabricants d'Eau de Javel"
     //original data series is incomplete e.g. 2.8, 2.9, 3.0, 3.5, 4.0
     //
-    var averageDensityForSolutionsFrom13HercentSodiumHypochlorite ={
+    var averageDensityForSolutionsFrom13PercentSodiumHypochlorite ={
       0.1:1.001,
       0.2:1.003,
       0.3:1.004,
@@ -71,7 +72,72 @@ var BleachSolution = function (options) {
       10.0:1.159
     }
 
-    this.originalDensity = averageDensityForSolutionsFrom13HercentSodiumHypochlorite[this.originalConcentration.toFixed(1)]
+    var averageDensityForSolutionsFrom24PercentSodiumHypochlorite ={
+      0.1:1.001,
+      0.2:1.002,
+      0.3:1.003,
+      0.4:1.004,
+      0.5:1.005,
+      0.6:1.006,
+      0.7:1.007,
+      0.8:1.008,
+      0.9:1.01,
+      1.0:1.011,
+      1.1:1.012,
+      1.2:1.013,
+      1.3:1.014,
+      1.4:1.015,
+      1.5:1.016,
+      1.6:1.017,
+      1.7:1.018,
+      1.8:1.019,
+      1.9:1.02,
+      2.0:1.021,
+      2.1:1.023,
+      2.2:1.024,
+      2.3:1.025,
+      2.4:1.026,
+      2.5:1.027,
+      2.6:1.028,
+      2.7:1.029,
+      2.8:1.03,
+      2.9:1.031,
+      3.0:1.033,
+      3.5:1.038,
+      4.5:1.05,
+      5.0:1.055,
+      5.5:1.061,
+      6.0:1.067,
+      6.5:1.073,
+      7.0:1.079,
+      7.5:1.086,
+      8.0:1.092,
+      8.5:1.098,
+      8.6:1.099,
+      8.7:1.101,
+      8.9:1.103,
+      9.0:1.105,
+      9.1:1.106,
+      9.2:1.107,
+      9.3:1.108,
+      9.4:1.11,
+      9.5:1.111,
+      9.6:1.112,
+      9.7:1.114,
+      9.8:1.115,
+      9.9:1.116,
+      10.0:1.118
+    }
+
+    this.originalDensity = function(){
+      if (this.manufacturingConcentration == 13){
+        return averageDensityForSolutionsFrom13PercentSodiumHypochlorite[this.originalConcentration.toFixed(1)]
+      }else if (this.manufacturingConcentration == 24) {
+        return averageDensityForSolutionsFrom24PercentSodiumHypochlorite[this.originalConcentration.toFixed(1)]
+      }else {
+         throw "Data only available for manufacturing densities of 13% or 24%"
+      }
+    }
 
     that = this;
     this.init();
@@ -118,7 +184,7 @@ BleachSolution.prototype.calculate = function(toCalc) {
 //9,6 x 1,152 x 10 = 110,59 grammes de chlore actif
 //
 BleachSolution.prototype.activeChlorine = function(){
-  activeChlorine = this.originalConcentration * this.originalDensity  * 10
+  activeChlorine = this.originalConcentration * this.originalDensity()  * 10
   return MathJS.round(activeChlorine, 2)
 }
 
--- test/index.js
+++ test/index.js
@@ -1,21 +1,54 @@
 var should = require('chai').should();
 var BleachSolution = require('../index.js');
+var MathJS = require('mathjs');
 
 
-//var s = new BleachSolution({originalConcentration : 20, finalVolume: 10, finalConcentration: 10});
-var s = new BleachSolution({originalConcentration: 0.087 , finalVolume: 10, finalConcentration: 1});
+//var s = new BleachSolution({originalConcentration : 20, finalVolume: 10, finalConcentration: 13});
+var s = new BleachSolution({originalConcentration: 0.087 , finalVolume: 10, finalConcentration: 1, manufacturingConcentration: 13});
 
 console.log ("Test4 OC1:" + s.originalConcentration);
-console.log("Test rounding: " + s.density2);
+console.log("Test mC: " + s.originalDensity());
+
+
+describe('manufacturing concentration', function(){
+  it('manufacturing concentration should default to 24%', function(){
+      solution = new BleachSolution({})
+      solution.manufacturingConcentration.should.equal(24)
+  });
+});
+
+describe('original density', function(){
+  it('should throw exception if manufacturing concentration is not 24% or 13%', function(){
+      solution = new BleachSolution({originalConcentration: 5.50, manufacturingConcentration: 19})
+      (function (solution) {solution.originalDensity()}).should.thow(Error, "Data only available for manufacturing densities of 13% or 24%")
+  });
+  it('should return original density for 13% original manufacturing concentration', function(){
+      solution = new BleachSolution({originalConcentration: 9.6, manufacturingConcentration: 13})
+      solution.originalDensity().should.equal(1.152)
+  });
+    it('should return original density for 24% original manufacturing concentration', function(){
+      solution = new BleachSolution({originalConcentration: 9.6, manufacturingConcentration: 24})
+      solution.originalDensity().should.equal(1.112)
+  });
+});
+
 
 describe('active chlorine calculation', function(){
   it('should calculate active chlorine for an original solution at 9.6% made from Sodium Hydrochloride with 13% acive chlorine', function(){
-      solution = new BleachSolution({originalConcentration: 9.6})
+      solution = new BleachSolution({originalConcentration: 9.6, manufacturingConcentration: 13})
       solution.activeChlorine().should.equal(110.59)
   });
     it('should calculate active chlorine for an original solution at 5.5% made from Sodium Hydrochloride with 13% acive chlorine', function(){
-      solution = new BleachSolution({originalConcentration: 5.50})
+      solution = new BleachSolution({originalConcentration: 5.50, manufacturingConcentration: 13})
       solution.activeChlorine().should.equal(59.51)
+  });
+    it('should calculate active chlorine for an original solution at 9.6% made from Sodium Hydrochloride with 24% acive chlorine', function(){
+      solution = new BleachSolution({originalConcentration: 2.6})
+      solution.activeChlorine().should.equal(26.73)
+  });
+    it('should calculate active chlorine for an original solution at 5.5% made from Sodium Hydrochloride with 24% acive chlorine', function(){
+      solution = new BleachSolution({originalConcentration: 9.10})
+      solution.activeChlorine().should.equal(100.65)
   });
 });
 
